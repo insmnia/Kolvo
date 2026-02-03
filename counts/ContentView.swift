e6590fa52956f2807f3aaad94c8824b31f8553df
y.blonski <yaraslau.blonski@gmail.com>
2026-02-03 14:33:02 +0300

    Initial Commit

---
+++ counts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  counts
+//
+//  Created by Yaraslau Blonski on 3.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ counts/countsApp.swift
@@ -0,0 +1,17 @@
+//
+//  countsApp.swift
+//  counts
+//
+//  Created by Yaraslau Blonski on 3.02.26.
+//
+
+import SwiftUI
+
+@main
+struct countsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
